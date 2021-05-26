1d93f084464f65ea31ebc1d87c75c680ebeaebbc
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2021-05-26 15:48:45 +0200

    finished the project

--- Form validator/app.js
+++ Form validator/app.js
@@ -6,13 +6,46 @@ const password2 = document.getElementById('password2')
 
 function showSuccess(input){
     const formControl = input.parentElement
+    formControl.classList.remove('error')
      formControl.classList.add('success')
 }
-function showError(input){
+function showError(input,message){
     const formControl = input.parentElement
-     formControl.classList.add('error')
+    formControl.querySelector('small').innerText = message 
+    formControl.classList.remove('success')
+    formControl.classList.add('error')
+    
+}
+
+function checkRequired(inputs){
+    inputs.forEach(input => {
+        if(input.value.trim() === '') showError(input,`${input.id} is required`)
+    })
+}
+function checkLength(fieldName,min,max){
+    if(fieldName.value.length < min){
+        showError(fieldName,`${fieldName.id} must be at least ${min} characters`)
+    }else if(fieldName.value.length > max){
+        showError(fieldName,`${fieldName.id} must be less than ${max} characters`)
+    }else showSuccess(fieldName)
+}
+
+function checkEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if(re.test(email.value)) showSuccess(email)
+    else showError(email,'email is not valid')
+}
+
+function checkPasswordsMatch(password1,password2){
+    if(password1.value !== password2.value) showError(password2,'passwords do not match')
+    else showSuccess(password2)
 }
 
 form.addEventListener('submit',e => {
     e.preventDefault()
+    checkLength(username,3,15)
+    checkLength(password,6,20)
+    checkEmail(email)
+    checkPasswordsMatch(password,password2)
+    checkRequired([username,email,password,password2])
 })
